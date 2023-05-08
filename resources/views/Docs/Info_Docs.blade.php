a31932f48f3e811ec3cc7cfd7cd98af7d21711eb
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-07 18:54:28 -0600

    solucion de errores commit tres

--- resources/views/Docs/Info_Docs.blade.php
+++ resources/views/Docs/Info_Docs.blade.php
@@ -8,7 +8,7 @@
           <h1 class="text-amber-500 text-center display-4 d-inline-block"><></h1>
           <h1 class="font text-6xl font-bold tracking-tight text-indigo-400 d-inline-block ml-4">Programación Web</h1>
           <br>
-          <p class="mt-4 text-xl text-gray-50">"En este blog personal de programación web, encontrarás información valiosa sobre el desarrollo de sitios web y aplicaciones en línea. Desde consejos y trucos para mejorar la eficiencia de tu código, hasta tutoriales detallados sobre el uso de las últimas tecnologías."</p>
+          <p class="mt-4 text-ml text-gray-50">"En este blog personal de programación web, encontrarás información valiosa sobre el desarrollo de sitios web y aplicaciones en línea. Desde consejos y trucos para mejorar la eficiencia de tu código, hasta tutoriales detallados sobre el uso de las últimas tecnologías."</p>
         </div>
       </div>
     </div>
