0cfa5e64a4d61fd2be8ce8c05382dbd0a60a5fe8
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-14 17:05:52 -0600

    solucion de errores commit tres

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -106,6 +106,24 @@ background-image: url(https://images.unsplash.com/photo-1574192324001-ee41e18ed6
     </head>
     <body  class="bg-personalizado">
       @include('NavBar/Nav')
+
+      <div class="relative overflow-hidden bg-transparent">
+        <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+          <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+            <div class="sm:max-w-lg">
+              <h1 class="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Summer styles are finally here</h1>
+              <p class="mt-4 text-xl text-gray-500">This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.</p>
+            </div>
+            <div>
+              <div class="mt-10">
+              
+      
+                <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">Shop Collection</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       
     </body>
 </html>
