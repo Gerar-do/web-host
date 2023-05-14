5889fd07ee0f13a02abf0dc249f1559beb97a470
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-14 17:59:47 -0600

    solucion de errores commit tres

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -95,9 +95,7 @@
 
 }
 .bg-personalizado{
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+
 background-image: url(https://images.unsplash.com/photo-1574192324001-ee41e18ed679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)
 }
 
@@ -127,74 +125,7 @@ background-image: url(https://images.unsplash.com/photo-1574192324001-ee41e18ed6
           </div>
         </div>
       </div>
-      <br>
-       <br>
-      <br>
-    <section class="bg-white">
-   
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 text-center  bg-white">
-      
-        <div class="overflow-hidden  py-12 sm:py-12  " >
-            <div class="mx-auto max-w-7xl px-6 lg:px-8">
-              <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                <div class="lg:pr-8 lg:pt-4">
-                  <div class="lg:max-w-lg">
-                    <h2 class="text-3xl font-semibold leading-7 text-gray-900">5 tips importantes de programación para mejorar tu flujo de trabajo</h2>
-                    <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 3a7 7 0 017 7v1a1 1 0 11-2 0v-1a5 5 0 00-5-5h-1a1 1 0 110-2h1zm0 4a3 3 0 100 6 3 3 0 000-6z" clip-rule="evenodd" />
-                                </svg>
-                                Utiliza atajos de teclado.
-                            </dt>
-                            <dd class="inline">Aprende los atajos de teclado de tu editor de texto para ser más eficiente al programar.</dd>
-                        </div>
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5 5a5 5 0 0110 0v2a5 5 0 01-10 0V5zm7 9a5 5 0 100-10 5 5 0 000 10zM5 10a5 5 0 1110 0 5 5 0 01-10 0z" clip-rule="evenodd" />
-                                </svg>
-                                Usa herramientas de automatización.
-                            </dt>
-                            <dd class="inline">Ahorra tiempo y reduce errores automatizando tareas repetitivas con herramientas como scripts y macros.</dd>
-                        </div>
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path d="M3 3a1 1 0 011-1h2.586a1 1 0 01.707.293l8.414 8.414a1 1 0 010 1.414l-8.414 8.414a1 1 0 01-1.414-1.414L13.586 11H4a1 1 0 01-1-1V3z" />
-                                </svg>
-                                Organiza tu código.
-                            </dt>
-                            <dd class="inline">Mantén tu código ordenado y fácil de leer utilizando convenciones de nomenclatura y estructuras lógicas.</dd>
-                        </div>
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5 5a5 5 0 0110 0v2a5 5 0 01-10 0V5zm7 9a5 5 0 100-10 5 5 0 000 10zM5 10a5 5 0 1110 0 5 5 0 01-10 0z" clip-rule="evenodd" />
-                                </svg>
-                                Usa control de versiones.
-                            </dt>
-                            <dd class="inline">Mantén un registro de cambios y versiones en tu código utilizando una herramienta de control de versiones como Git, esto te permitirá trabajar en equipo de manera eficiente y tener un historial de cambios.</dd>
-                        </div>
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 3a7 7 0 017 7v1a1 1 0 11-2 0v-1a5 5 0 00-5-5h-1a1 1 0 110-2h1zm0 4a3 3 0 100 6 3 3 0 000-6z" clip-rule="evenodd" />
-                                </svg>
-                                Documenta tu código.
-                            </dt>
-                            <dd class="inline">Agrega comentarios y documentación a tu código para que otros desarrolladores puedan entenderlo fácilmente y también te ayudará a recordar lo que hiciste en el futuro.</dd>
-                        </div>
-                  </div>
-                </div>
-                <img src="https://images.pexels.com/photos/6424583/pexels-photo-6424583.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[61rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
-              </div>
-            </div>
-              </div>
-         </div>
-    </section>
-  
+  @include("Docs/Info_Docs")
       @include('Footer/Footer')
     </body>
 </html>
