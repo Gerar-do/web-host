ad5759d4a2125a61a62dd90c55928a72811cd9d1
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-14 17:22:13 -0600

    solucion de errores commit tres

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -111,14 +111,14 @@ background-image: url(https://images.unsplash.com/photo-1574192324001-ee41e18ed6
         <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div class="sm:max-w-lg">
-              <h1 class="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Summer styles are finally here</h1>
-              <p class="mt-4 text-xl text-gray-500">This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.</p>
+              <h1 class="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">web programming blog</h1>
+              <p class="mt-4 text-xl text-gray-500">A programming web blog is a website that provides informative and educational articles on various aspects of web development, primarily aimed at a programming  audience Front-end.</p>
             </div>
             <div>
               <div class="mt-10">
               
       
-                <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">Shop Collection</a>
+                <a href="#" class="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">proximamente</a>
               </div>
             </div>
           </div>
