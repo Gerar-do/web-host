f81cba17001c653dc49958ee7e83fec9859e5775
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-14 17:48:24 -0600

    solucion de errores commit tres

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -95,6 +95,9 @@
 
 }
 .bg-personalizado{
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 background-image: url(https://images.unsplash.com/photo-1574192324001-ee41e18ed679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)
 }
 
