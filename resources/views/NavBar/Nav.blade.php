c0a358142b10606a5ba6c8b67287cbfaff2bc7cc
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-05-15 16:55:14 -0600

    solucion de errores commit tres

--- resources/views/NavBar/Nav.blade.php
+++ resources/views/NavBar/Nav.blade.php
@@ -8,14 +8,7 @@
    
   
     <ul class="nav justify-content-center">
-      <li class="nav-item">
-  <a class="nav-link  d-flex align-items-center  text-decoration-none hover:text-indigo-200 text-gray-50" href="{{ route('Home') }}">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door me-2" viewBox="0 0 16 16">
-                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
-              </svg>
-              Home
-            </a>
-      </li>
+     
       <li class="nav-item">
   <a class="nav-link active d-flex  text-decoration-none hover:text-indigo-200 text-gray-50" href="#">
     
@@ -39,10 +32,15 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
     
-          
+          <a class="nav-link justify-content-center  d-flex align-items-center text-center text-decoration-none hover:text-indigo-200 text-gray-50" href="{{ route('Home') }}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door me-2" viewBox="0 0 16 16">
+              <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+            </svg>
+            Home
+          </a>
         
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle  text-center" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-center " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Topics about web programming
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
